42d2ee0257b33a3ba444277964e252875e890af0
GmoyanoN <guilloxg22@gmail.com>
2021-05-16 18:19:05 -0300

    Initial commit

---
+++ src/main/java/utn/frsf/isi/dan/grupotp/tplab/danmsproductos/Application.java
@@ -0,0 +1,13 @@
+package utn.frsf.isi.dan.grupotp.tplab.danmsproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/utn/frsf/isi/dan/grupotp/tplab/danmsproductos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frsf.isi.dan.grupotp.tplab.danmsproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
